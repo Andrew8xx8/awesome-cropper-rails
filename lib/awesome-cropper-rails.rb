52eee4b64b09f2f01264e54fa5b01134aec1a974
Andrew8xx8 <avk@8xx8.ru>
2013-01-29 15:01:23 +0400

    Simple form input removed

--- lib/awesome-cropper-rails.rb
+++ lib/awesome-cropper-rails.rb
@@ -1,5 +1,1 @@
-require 'simple_form'
-require 'awesome-cropper/awesome_cropper_input.rb'
 require "awesome-cropper/rails"
-
-SimpleForm::Inputs.send(:include, AwesomeCropper)
--- lib/awesome-cropper/awesome_cropper_input.rb
+++
@@ -1,7 +0,0 @@
-module AwesomeCropper
-  class AwesomeCropperInput < SimpleForm::Inputs::FileInput
-    def input
-      "<input class=\"awesome-cropper-input\" type=\"hidden\" name=\"#{lookup_model_names}[#{attribute_name}]\">"
-    end
-  end
-end
