d216d17761a24a0c73fba1ac8e4a759c5d308413
Andrew8xx8 <avk@8xx8.ru>
2013-01-29 14:01:42 +0400

    Model name fixed

--- lib/awesome-cropper/awesome_cropper_input.rb
+++ lib/awesome-cropper/awesome_cropper_input.rb
@@ -1,7 +1,7 @@
 module AwesomeCropper
   class AwesomeCropperInput < SimpleForm::Inputs::FileInput
     def input
-      "<input class=\"awesome-cropper-input\" type=\"hidden\" name=\"{#lookup_model_names}[#{attribute_name}]\">"
+      "<input class=\"awesome-cropper-input\" type=\"hidden\" name=\"#{lookup_model_names}[#{attribute_name}]\">"
     end
   end
 end
