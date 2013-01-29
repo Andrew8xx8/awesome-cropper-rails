81ec559449544734379cc7ae8b71b6208b367452
Andrew8xx8 <avk@8xx8.ru>
2013-01-29 14:45:03 +0400

    SimpleForm initialization moved

--- lib/awesome-cropper-rails.rb
+++ lib/awesome-cropper-rails.rb
@@ -1,3 +1,5 @@
-require 'simple_form' 
-require 'awesome-cropper/awesome_cropper_input.rb' 
+require 'simple_form'
+require 'awesome-cropper/awesome_cropper_input.rb'
 require "awesome-cropper/rails"
+
+SimpleForm::Inputs.send(:include, AwesomeCropper)
--- lib/awesome-cropper/rails.rb
+++ lib/awesome-cropper/rails.rb
@@ -1,8 +1,5 @@
-require 'simple_form'
 require 'awesome-cropper/rails/engine' if ::Rails.version >= '3.1'
 require 'awesome-cropper/rails/version'
-
-SimpleForm::Inputs.send(:include, AwesomeCropper)
 
 module AwesomeCropper
   module Rails
